48e0ae5452dc53606fea3add392298bb1ab32b7d
Marco Concetto Rudilosso <marco.rudilosso@shopify.com>
2023-10-10 18:27:40 +0200

    use an option to contain the result

--- src/lib.rs
+++ src/lib.rs
@@ -34,27 +34,27 @@ macro_rules! call_without_gvl {
     ($func:expr, args: ($($arg:expr, $ty:ty),+), return_type: $return_ty:ty) => {
         {
             lucchetto::paste::paste! {
-                let mut result: $return_ty = Default::default();
+                let mut result: Option<$return_ty> = None;
                 $(lucchetto::static_assertions::assert_impl_all!($ty: lucchetto::GvlSafe);)+
                 lucchetto::static_assertions::assert_impl_all!($return_ty: lucchetto::GvlSafe);
-                let data: ( $($ty,)+ *mut $return_ty ) = ($($arg,)+ &mut result);
+                let data: ( $($ty,)+ &mut Option<$return_ty> ) = ($($arg,)+ &mut result);
 
                 let data_ptr: *mut std::ffi::c_void = &data as *const _ as *mut _;
                 std::mem::forget(data);
 
                 unsafe extern "C" fn __decl_macro_anon_wrapper(data: *mut std::ffi::c_void) -> *mut std::ffi::c_void {
-                        let data = data as *mut ( $($ty,)+ *mut $return_ty );
+                        let data = data as *mut ( $($ty,)+ &mut Option<$return_ty> );
 
                         let ( $([<__ $arg _name>],)+ result_output) = data.read();
                         let result = $func( $( [<__ $arg _name>], )+);
-                        std::ptr::write(result_output, result);
+                        *result_output = Some(result);
                         std::ptr::null_mut()
                     }
 
                 unsafe {
                     lucchetto::rb_sys::rb_thread_call_without_gvl(Some(__decl_macro_anon_wrapper), data_ptr, None, std::ptr::null_mut());
                 }
-                result
+                result.unwrap()
             }
         }
     };
